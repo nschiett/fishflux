6b203841f539acc83e0f968f8fc11dd81c2a26df
nschiett <nina.schiettekatte@gmail.com>
2020-06-24 17:09:19 +0200

    fix :bug:

--- tests/testthat/test-cnp_model_mcmc.R
+++ tests/testthat/test-cnp_model_mcmc.R
@@ -9,6 +9,6 @@ test_that("Simple corner cases", {
     expect_error(cnp_model_mcmc(TL = 10))
     expect_error(cnp_model_mcmc(TL = 10, param = list(Qc_m = -40, Qn_m = 10, Qp_m = 4, theta_m = 3)))
     expect_equal(length(model), 2)
-    expect_is(model[[1]], "list")
+    expect_is(model[[1]], "stanfit")
     expect_is(model[[2]], "data.frame")
 })
