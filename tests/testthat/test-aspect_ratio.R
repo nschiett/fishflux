665ff9a3b90923ac9bfc9da3a17bc8d73797e122
Diego Barneche <diego.barneche@ymail.com>
2020-06-27 11:09:39 +0800

    in aspect_ratio, level column is not factor

--- tests/testthat/test-aspect_ratio.R
+++ tests/testthat/test-aspect_ratio.R
@@ -7,6 +7,6 @@ test_that("Simple corner cases", {
   expect_equal(ar, fishflux::aspect_ratio("Zebrasoma scopas"))
   expect_length(ar, 3)
   expect_equal(as.character(ar[1, 1]), "Zebrasoma scopas")
-  expect_true(is.factor(ar[1, 3]))
+  expect_true(!is.factor(ar[1, 3]))
   expect_gt(ar[1, 2], 0)
 })
