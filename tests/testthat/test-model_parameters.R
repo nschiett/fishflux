580499ad4402d8448c700f72878253468274f823
nschiett <nina.schiettekatte@gmail.com>
2020-06-27 15:38:28 +0200

    version 0.1.0 :fish: :tada: :sparkles:

---
+++ tests/testthat/test-model_parameters.R
@@ -0,0 +1,23 @@
+# example
+mp <- fishflux::model_parameters(sp = "Scarus psittacus", family = "Scaridae", temp = 27)
+
+
+test_that("Simple corner cases", {
+  expect_length(mp, 15)
+  expect_equal(nrow(mp), 1)
+  expect_true(!is.numeric(mp$species))
+  expect_true(is.numeric(mp$t0))
+  expect_true(is.numeric(mp$Linf))
+  expect_true(is.numeric(mp$k))
+  expect_true(is.numeric(mp$asp))
+  expect_true(is.numeric(mp$troph))
+  expect_true(is.numeric(mp$lwa_m))
+  expect_true(is.numeric(mp$lwa_sd))
+  expect_true(is.numeric(mp$lwb_m))
+  expect_true(is.numeric(mp$lwb_sd))
+  expect_true(is.numeric(mp$mdw_m))
+  expect_true(is.numeric(mp$f0_m))
+  expect_true(is.numeric(mp$f0_sd))
+  expect_true(is.numeric(mp$alpha_m))
+  expect_true(is.numeric(mp$alpha_sd))
+})
---
+++ tests/testthat/test-trophic_level.R
@@ -0,0 +1,11 @@
+# example
+l <- fishflux::trophic_level("Zebrasoma scopas")
+
+test_that("Simple corner cases", {
+  expect_gt(min(l$trophic_level), 0)
+  expect_length(l, 3)
+  expect_equal(nrow(l), 1)
+  expect_true(is.numeric(l$trophic_level))
+  expect_true(!is.numeric(l$species))
+  expect_true(!is.numeric(l$level))
+})
