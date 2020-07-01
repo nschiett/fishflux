19c5dd8f98a7960e856a6842fa837c0330884868
nschiett <nina.schiettekatte@gmail.com>
2020-07-01 16:14:55 +0200

    description section edits

--- R/cnp_model_mcmc.R
+++ R/cnp_model_mcmc.R
@@ -33,8 +33,7 @@
 #' @param cor     A list of correlations between certain parameters: ro_Qc_Qn, ro_Qc_Qp, ro_Qn_Qp,
 #' ro_Dc_Dn, ro_Dc_Dp, ro_Dn_Dp, ro_lwa_lwb, ro_alpha_f0
 #' @param iter    A positive integer specifying the number of iterations. The default is 2000.
-#' @param ... Additional arguments to 
-#'   \code{\link[rstan]{sampling}} 
+#' @param ... Additional arguments rstan::sampling, see ?rstan:sampling
 #'
 #' @details Returns a list with two objects: A stanfit object and a data.frame with a summary of all model components.
 #'  See \code{\link{extract}} to extract a summary of predicted variables and
